07b32501343859f527a928e9f3f501f9d1d823f8
Нурлан <@->
2022-10-28 21:37:36 +0400

    Добавьте файлы проекта.

---
+++ chic2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chic2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D;
+
+            try
+            {
+                Console.WriteLine("Введите 4 числа");
+                A = Convert.ToInt32(Console.ReadLine());
+                B = Convert.ToInt32(Console.ReadLine());
+                C = Convert.ToInt32(Console.ReadLine());
+                D = Convert.ToInt32(Console.ReadLine());
+                if (A == B && C == D && A == C && B == D)
+                    Console.WriteLine("числа совпадают");
+                else
+                    Console.WriteLine("числа разные");
+                    Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Введите целое число");
+                Console.ReadKey();
+            }
+
+            
+        }
+    }
+}
